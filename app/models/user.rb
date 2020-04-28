ca9f189e9126ef73e344412a2e699394b2bf3549
Ivan Diaz <ivanid22@gmail.com>
2020-04-28 16:32:43 -0300

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
+  validates :password, presence: true, length: {maximum: 8}
+   
+end
